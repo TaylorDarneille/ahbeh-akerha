d7e892ab587f99cab62e870a58e8353d726d6891
TaylorDarneille <tmdarneille@gmail.com>
2022-10-16 10:14:05 +0300

    favorite animals complete

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const ejsLayouts = require('express-ejs-layouts')
 
 // MIDDLEWARE
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 // ROUTES
+// home
+app.get('/', (req, res)=>{
+    res.render('home.ejs')
+})
+// favorite animals
+app.get('/animals', (req, res)=>{
+    const faveAnimals = ['sand crab', 'corny joke dog', 'benedict the sea cucumber']
+    res.render('animals.ejs', {animals: faveAnimals})
+})
 
 app.listen(PORT, ()=>{
     console.log(`You're running the ahbeh-akerha app`)
